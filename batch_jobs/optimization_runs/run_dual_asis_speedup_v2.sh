f5c502711a6f3d87bbaaa5702d95bb635fe7761e
Bertram Hage <b.hage@me.com>
2026-05-24 14:45:03 +0200

    add batch jobs

---
+++ batch_jobs/optimization_runs/run_dual_asis_speedup_v2.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+### Job Name:
+#BSUB -J run_replication_speedup_v2
+
+### Queue Name:
+#BSUB -q hpc
+
+### Requesting 4 CPU cores, 4GB memory per core
+#BSUB -n 4
+#BSUB -R "rusage[mem=4GB]"
+
+### Setting a runtime limit of 48 hours
+#BSUB -W 48:00
+
+### Email notification when job begins and ends
+#BSUB -B
+#BSUB -N
+
+### Output and error files
+#BSUB -o batch_jobs/logs/Output_%J.out
+#BSUB -e batch_jobs/logs/Output_%J.err
+
+
+### cd to repo dir
+cd ~/deep_rl_project
+
+mkdir -p batch_jobs/logs
+export CPLEX_PATH="/apps/cplex/cplex1210/opl/bin/x86-64_linux/"
+
+### activate environment
+. .venv/bin/activate
+
+### multi agent
+python main_a2c_multi_agent.py --city nyc_man_south --mode 2 --checkpoint_path my_dual_agent_speedup_v2 --wandb_group optimization_runs
